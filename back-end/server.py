c75606b15302ea04a8de57362f78e268b47d3b3b
TSKailash <tskailash20@gmail.com>
2025-04-08 23:13:30 +0530

    feat

--- back-end/server.py
+++ back-end/server.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask Server is running!"
+client = MongoClient('mongodb+srv://tskailash20:tskailashts0807%40%40@cluster0.io3sx5a.mongodb.net/edu_tech?retryWrites=true&w=majority&appName=Cluster0')
+db = client['mydatabase']
+collection = db['users']  
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    try:
+        # Force JSON parsing even if Content-Type is wrong
+        data = request.get_json(force=True)
+
+        name = data.get('name')
+        age = data.get('age')
+
+        if not name or not age:
+            return jsonify({"error": "Missing data"}), 400
+
+        user = {"name": name, "age": age}
+        collection.insert_one(user)
+
+        return jsonify({"message": "User added successfully"}), 201
+
+    except Exception as e:
+        return jsonify({"error": f"Failed to add user: {str(e)}"}), 500
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = list(collection.find({}, {"_id": 0}))
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
