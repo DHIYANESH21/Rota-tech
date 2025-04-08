5d2e3e1599c04c76b0737f54c7e2143fbc2512e8
Kabil081 <kabilram212@gmail.com>
2025-04-08 22:28:28 +0530

    modified server.py

--- back-end/server.py
+++ back-end/server.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+MONGO_URI = "mongodb+srv://kabilrsit:awcgkyuCJCgmStHy@projects.vifrcka.mongodb.net/"
+client = MongoClient(MONGO_URI)
+
+try:
+    client.admin.command('ping')
+    print("✅ MongoDB connection successful!")
+except Exception as e:
+    print("❌ MongoDB connection failed:", e)
+
+db = client['mydatabase']
+collection = db['users']
+
 @app.route('/')
 def home():
-    return "Hello, Flask Server is running!"
+    return "Flask + MongoDB Atlas is working!"
+
+@app.route('/users')
+def get_users():
+    users = list(collection.find({}, {'_id': 0}))  
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
